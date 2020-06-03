a1e3aad1884f0a38a62886a4c57f26f84057fc6f
Boah Lee <boahlee@hotmail.com>
2020-06-03 10:21:00 +1000

    added header component and links to home and shop page, also added contact option

--- src/App.js
+++ src/App.js
@@ -6,16 +6,12 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-
-// const JohnnyPage = () => (
-//   <div>
-//     <h1>Johnny Page</h1>
-//   </div>
-// );
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
